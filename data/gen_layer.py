794f787f214c63f7184718081c9cff362aeb646c
wangshy31 <450681821@qq.com>
2018-08-21 16:10:48 +0800

    toold for generating prototxt

---
+++ data/gen_layer.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+
+def generate_data_layer_str(label_source, data_source, local_dict,train_batch_size, test_batch_size,
+                            channel, train_num_words, test_num_words, c_h, c_w):
+    train_label = label_source+'/train_label.txt'
+    train_data = data_source+'/train_data.txt'
+    test_label = label_source+'/test_label.txt'
+    test_data = data_source+'/test_data.txt'
+
+    data_layer_str = '''name: "DenseAttentionNet"
+layer {
+  name: "data"
+  type: "TextData"
+  top: "data"
+  top: "label"
+  include {
+    phase: TRAIN
+  }
+  text_data_param {
+    label_source: "%s"
+    data_source: "%s"
+    dict_source: "%s"
+    batch_size: %d
+    channel: %d
+    num_words: %d
+    crop_height: %d
+    crop_width: %d
+  }
+}
+layer {
+  name: "data"
+  type: "TextData"
+  top: "data"
+  top: "label"
+  include {
+    phase: TEST
+  }
+  text_data_param {
+    label_source: "%s"
+    data_source: "%s"
+    dict_source: "%s"
+    batch_size: %d
+    channel: %d
+    num_words: %d
+    crop_height: %d
+    crop_width: %d
+    shuffle: 0
+  }
+}\n'''%(train_label, train_data, local_dict, train_batch_size, channel, train_num_words, c_h, c_w,
+        test_label, test_data, local_dict, test_batch_size, channel, test_num_words, c_h, c_w)
+    return data_layer_str
+
+def generate_conv_layer_str(name, bottom, top, num_output, kernel_h, kernel_w, pad_h, pad_w, num_groups=1):
+    conv_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Convolution"
+  convolution_param {
+    num_output: %d
+    stride: 1
+    weight_filler {
+      type: "gaussian"
+      std: 0.058925565
+    }
+    bias_filler {
+      type: "constant"
+      value: 0
+    }
+    group: %d
+    kernel_h: %d
+    kernel_w: %d
+    pad_h: %d
+    pad_w: %d
+  }
+}\n'''%(name, bottom, top, num_output, num_groups, kernel_h, kernel_w, pad_h, pad_w)
+    return conv_layer_str
+
+def generate_bn_layer_str(bn_name, bottom, top):
+    bn_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "BatchNorm"
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+}\n'''%(bn_name, bottom, top)
+    return bn_layer_str
+
+def generate_activation_layer_str(name, bottom, act_type="ReLU"):
+    act_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "%s"
+}\n'''%(name, bottom, bottom, act_type)
+    return act_layer_str
+
+def generate_concat_layer_str(name, bottoms, top, concat_axis=1):
+    bottom_str = ''
+    for item in bottoms:
+        bottom_str += '  bottom: "'+item+'"\n'
+    concat_layer_str = '''layer {
+  name: "%s"
+%s  top: "%s"
+  type: "Concat"
+  concat_param {
+    axis: %d
+  }
+}\n'''%(name, bottom_str, top, concat_axis)
+    return concat_layer_str
+
+def generate_slice_layer_str(name, bottom, top, num_slice, num_dimension, axis=1):
+    top_str = ''
+    slice_point_str = '    axis: '+str(axis)+'\n'
+    for i in range(num_slice):
+        top_str += '  top: "' + top + str(i)+'"\n'
+    for i in range(1, num_slice):
+        slice_point_str += '    slice_point: ' + str(i*num_dimension) + '\n'
+    slice_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+%s  type: "Slice"
+  slice_param {
+%s    }
+}\n'''%(name, bottom, top_str, slice_point_str)
+    return slice_layer_str
+
+def generate_permute_layer_str(name, bottom, top, permute_param_list):
+    permute_param_str = 'permute_param {\n'
+    for i in permute_param_list:
+        permute_param_str += '      order: ' + str(i) + '\n'
+    permute_param_str += '  }'
+
+    permute_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Permute"
+  %s
+}\n'''%(name, bottom, top, permute_param_str)
+    return permute_layer_str
+
+def generate_reduction_layer_str(name, bottom, top, reduction_axis):
+    reduction_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Reduction"
+  reduction_param {
+    axis: %d
+    }
+}\n'''%(name, bottom, top, reduction_axis)
+    return reduction_layer_str
+
+def generate_tile_layer_str(name, bottom, top, axis, tiles):
+    tile_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Tile"
+  tile_param {
+    axis: %d
+    tiles: %d
+  }
+}\n'''%(name, bottom, top, axis, tiles)
+    return tile_layer_str
+
+def generate_reshape_layer_str(name, bottom, top, reshape_param_list):
+    reshape_param_str = 'reshape_param {\n    shape {\n'
+    for i in reshape_param_list:
+        reshape_param_str += '      dim: ' + str(i) + '\n'
+    reshape_param_str += '    }\n  }'
+
+    reshape_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Reshape"
+  %s
+}\n'''%(name, bottom, top, reshape_param_str)
+    return reshape_layer_str
+
+def generate_eltwise_layer_str(name, bottoms, top, operation):
+    bottom_str = ''
+    for item in bottoms:
+        bottom_str += '  bottom: "'+item+'"\n'
+    eltwise_layer_str = '''layer {
+  name: "%s"
+%s  top: "%s"
+  type: "Eltwise"
+  eltwise_param {
+    operation: %s
+  }
+}\n'''%(name, bottom_str, top, operation)
+    return eltwise_layer_str
+
+def generate_pooling_layer_str(name, bottom, top, pool_type="AVE", kernel_h=3, kernel_w=1, stride_h=2, stride_w=1, pad_h=1, pad_w=0):
+    if pool_type == 'global_pooling':
+        pool_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Pooling"
+  pooling_param {
+    global_pooling : true
+  }
+}\n'''%(name, bottom, top)
+    else:
+        pool_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Pooling"
+  pooling_param {
+    pool: %s
+    kernel_h: %d
+    kernel_w: %d
+    stride_h: %d
+    stride_w: %d
+    pad_h: %d
+    pad_w: %d
+  }
+}\n'''%(name, bottom, top, pool_type, kernel_h, kernel_w, stride_h, stride_w, pad_h, pad_w)
+    return pool_layer_str
+
+def generate_fc_layer_str(name, bottom, top, num_output, filler="msra", std=0.01):
+    fc_layer_str = '''layer {
+  name: "%s"
+  type: "InnerProduct"
+  bottom: "%s"
+  top: "%s"
+  param { lr_mult: 1 decay_mult: 1 }
+  param { lr_mult: 2 decay_mult: 0 }
+  inner_product_param {
+     num_output: %d
+     weight_filler { type: "%s" std: %.3f }
+     bias_filler { type: "constant" value: 0 }
+  }
+}\n'''%(name, bottom, top, num_output, filler, std)
+    return fc_layer_str
+
+def generate_flatten_layer_str(name, bottom, top):
+    flatten_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Flatten"
+}\n'''%(name, bottom, top)
+    return flatten_layer_str
+
+
+def generate_dropout_layer_str(name, bottom, top, dropout_ratio):
+    dropout_layer_str = '''layer {
+  name: "%s"
+  bottom: "%s"
+  top: "%s"
+  type: "Dropout"
+  dropout_param {
+    dropout_ratio: %f
+  }
+}\n'''%(name, bottom, top, dropout_ratio)
+    return dropout_layer_str
+
+
+def generate_softmax_layer_str(name, bottom, top):
+    softmax_layer_str = '''layer {
+  name: "%s"
+  type: "Softmax"
+  bottom: "%s"
+  top: "%s"
+}\n'''%(name, bottom, top)
+    return softmax_layer_str
+
+def generate_softmax_loss_str(name, bottom0, bottom1, top):
+    softmax_loss_str = '''layer {
+  name: "%s"
+  type: "SoftmaxWithLoss"
+  bottom: "%s"
+  bottom: "%s"
+  top: "%s"
+}\n'''%(name, bottom0, bottom1, top)
+    return softmax_loss_str
+
+def generate_accuracy_str(name, bottom0, bottom1, top):
+    accuracy_str = '''layer {
+  name: "%s"
+  type: "Accuracy"
+  bottom: "%s"
+  bottom: "%s"
+  top: "%s"
+}\n'''%(name, bottom0, bottom1, top)
+    return accuracy_str
+
---
+++ data/gen_model.py
@@ -0,0 +1,181 @@
+import gen_layer
+import math
+
+label_source = "/ssd/ijcai18/data/agnews"
+data_source = "/ssd/ijcai18/data/agnews"
+local_dict_source = "/ssd/ijcai18/data/agnews/local_dic_index.txt"
+num_trans_layers = 3
+num_dense_layers = 5
+num_class = 4
+use_pooling = True
+use_ip = True
+
+#change the line below to experiment with different setting
+#depth -- must be 3n+4
+#first_output -- #channels before entering the first dense block, set it to be comparable to growth_rate
+#growth_rate -- growth rate
+#dropout -- set to 0 to disable dropout, non-zero number to set dropout rate
+def gen_data_layer(f, label_source=label_source, data_source=data_source,
+                   local_dict=local_dict_source,train_batch_size=64,
+                   test_batch_size=80, channel=1, train_num_words=200,
+                   test_num_words=100, crop_height=100, crop_width=300):
+    f.write(gen_layer.generate_data_layer_str(
+        label_source, data_source, local_dict,train_batch_size, test_batch_size,
+        channel, train_num_words, test_num_words,crop_height, crop_width))
+
+def gen_trans_layer(f, num_layers=num_trans_layers, num_output=64, kernel_h=1,
+                    kernel_w=1, pad_h=0, pad_w=0):
+    #generate (conv+bn+relu)*num_layers
+    f.write('#----Transform Feature Dimension----\n')
+    for i in range(num_layers):
+        if i == 0:
+            f.write(gen_layer.generate_conv_layer_str(
+                'trans_layer_conv_'+str(i), 'data', 'trans_layer_conv_'+str(i), num_output, 1, 300, 0, 0))
+        else:
+            f.write(gen_layer.generate_conv_layer_str(
+                'trans_layer_conv_'+str(i), 'trans_layer_bn_'+str(i-1), 'trans_layer_conv_'+str(i),
+                num_output, kernel_h, kernel_w, pad_h, pad_w))
+
+        f.write(gen_layer.generate_bn_layer_str(
+            'trans_layer_bn_'+str(i), 'trans_layer_conv_'+str(i), 'trans_layer_bn_'+str(i)))
+        f.write(gen_layer.generate_activation_layer_str(
+            'trans_layer_relu_'+str(i), 'trans_layer_bn_'+str(i)))
+
+def gen_dense_layer(f, num_layers=num_dense_layers, num_output=64, kernel_h=3,
+                    kernel_w=1, pad_h=1, pad_w=0, use_pooling=use_pooling):
+    #generate (conv+concat+bn+relu)*num_layers
+    f.write('#----Dense CNN----\n')
+    for i in range(num_layers):
+        if i == 0:
+            #conv
+            f.write(gen_layer.generate_conv_layer_str(
+                'dense_layer_conv_'+str(i), 'trans_layer_bn_'+str(num_trans_layers-1), 'dense_layer_conv_'+str(i),
+                num_output, kernel_h, kernel_w, pad_h, pad_w))
+
+            #concat
+            bottom_list = ['dense_layer_conv_0']
+            for j in range(num_trans_layers):
+                bottom_list.append('trans_layer_conv_'+str(j))
+            f.write(gen_layer.generate_concat_layer_str('dense_layer_concat_'+str(i), bottom_list,
+                                                      'dense_layer_concat_'+str(i)))
+        else:
+            #conv
+            if use_pooling:
+                f.write(gen_layer.generate_conv_layer_str(
+                    'dense_layer_conv_'+str(i), 'dense_layer_pool_'+str(i-1), 'dense_layer_conv_'+str(i),
+                    num_output, kernel_h, kernel_w, pad_h, pad_w))
+            else:
+                f.write(gen_layer.generate_conv_layer_str(
+                    'dense_layer_conv_'+str(i), 'dense_layer_bn_'+str(i-1), 'dense_layer_conv_'+str(i),
+                    num_output, kernel_h, kernel_w, pad_h, pad_w))
+
+            #concat
+            if use_pooling:
+                bottom_list = ['dense_layer_conv_'+str(i), 'dense_layer_pool_'+str(i-1)]
+                f.write(gen_layer.generate_concat_layer_str('dense_layer_concat_'+str(i), bottom_list,
+                                                      'dense_layer_concat_'+str(i)))
+            else:
+                bottom_list = ['dense_layer_conv_'+str(i), 'dense_layer_bn_'+str(i-1)]
+                f.write(gen_layer.generate_concat_layer_str('dense_layer_concat_'+str(i), bottom_list,
+                                                      'dense_layer_concat_'+str(i)))
+        #bn
+        f.write(gen_layer.generate_bn_layer_str(
+            'dense_layer_bn_'+str(i), 'dense_layer_concat_'+str(i), 'dense_layer_bn_'+str(i)))
+        #relu
+        f.write(gen_layer.generate_activation_layer_str(
+            'dense_layer_relu_'+str(i), 'dense_layer_bn_'+str(i)))
+        #pooling
+        if use_pooling and i < num_layers-1:
+            f.write(gen_layer.generate_pooling_layer_str(
+                'dense_layer_pool_'+str(i), 'dense_layer_bn_'+str(i), 'dense_layer_pool_'+str(i)))
+
+def gen_attention_layer(f, num_layers=3, num_output=64, kernel_h=1, kernel_w=1, pad_h=0, pad_w=0):
+    f.write('#----Multi-scale Feature Attention----\n')
+    def calc_pooled_height(x):
+        for i in range(num_dense_layers - 1):
+            x = math.ceil(0.5*(x - 1)) +1
+        return int(x)
+    num_height = calc_pooled_height(100)
+    num_group = num_trans_layers + num_dense_layers
+
+    #group conv + filter ensemble + scale reweight
+    #use group convolution as bottlenecks
+    for i in range(num_layers):
+        #conv
+        if i == 0:
+            f.write(gen_layer.generate_conv_layer_str(
+                'attention_layer_conv_'+str(i), 'dense_layer_bn_'+str(num_dense_layers-1), 'attention_layer_conv_'+str(i),
+                num_output*num_group, kernel_h, kernel_w, pad_h, pad_w, num_group))
+        else:
+            f.write(gen_layer.generate_conv_layer_str(
+                'attention_layer_conv_'+str(i), 'attention_layer_bn_'+str(i-1), 'attention_layer_conv_'+str(i),
+                num_output*num_group, kernel_h, kernel_w, pad_h, pad_w, num_group))
+        #bn
+        f.write(gen_layer.generate_bn_layer_str(
+            'attention_layer_bn_'+str(i), 'attention_layer_conv_'+str(i), 'attention_layer_bn_'+str(i)))
+        #relu
+        f.write(gen_layer.generate_activation_layer_str(
+            'attention_layer_relu_'+str(i), 'attention_layer_bn_'+str(i)))
+
+    #filter ensemble
+    f.write(gen_layer.generate_slice_layer_str('attention_scale_slice', 'attention_layer_bn_'+str(num_layers-1),
+                                               'attention_scale_', num_group, num_output))
+    for i in range(num_group):
+        f.write(gen_layer.generate_permute_layer_str('attention_permute_'+str(i), 'attention_scale_'+str(i),
+                                               'attention_permute_'+str(i), [0, 3, 2, 1]))
+        f.write(gen_layer.generate_reduction_layer_str('attention_reduction_'+str(i), 'attention_permute_'+str(i),
+                                               'attention_reduction_'+str(i), 3))
+    bottom_list = []
+    for i in range(num_group):
+        bottom_list.append('attention_reduction_'+str(i))
+    f.write(gen_layer.generate_concat_layer_str('attention_scale_concat', bottom_list, 'attention_scale_concat'))
+
+    #scale reweight
+    f.write(gen_layer.generate_slice_layer_str('attention_height_slice', 'attention_scale_concat',
+                                               'attention_height_', num_height, 1, 2))
+    for i in range(num_height):
+        f.write(gen_layer.generate_fc_layer_str('attention_height_fc1_'+str(i), 'attention_height_'+str(i),
+                                               'attention_height_fc1_'+str(i), num_group))
+        f.write(gen_layer.generate_fc_layer_str('attention_height_fc2_'+str(i), 'attention_height_fc1_'+str(i),
+                                               'attention_height_fc2_'+str(i), num_group))
+        f.write(gen_layer.generate_reshape_layer_str('attention_height_reshape_'+str(i), 'attention_height_fc2_'+str(i),
+                                               'attention_height_reshape_'+str(i), [0, 0, 1, 1]))
+    bottom_list = []
+    for i in range(num_height):
+        bottom_list.append('attention_height_reshape_'+str(i))
+    f.write(gen_layer.generate_concat_layer_str('attention_height_concat', bottom_list, 'attention_height_concat', 2))
+    f.write(gen_layer.generate_softmax_layer_str('attention_weight', 'attention_height_concat', 'attention_weight'))
+    f.write(gen_layer.generate_slice_layer_str('attention_weight_slice', 'attention_weight',
+                                               'attention_weight_slice_', num_group, 1))
+    bottom_list = []
+    for i in range(num_group):
+        f.write(gen_layer.generate_tile_layer_str('attention_weight_tile_'+str(i), 'attention_weight_slice_'+str(i),
+                                                  'attention_weight_tile_'+str(i), 1, 64))
+        f.write(gen_layer.generate_eltwise_layer_str('attention_reweight_'+str(i), ['attention_scale_'+str(i), 'attention_weight_tile_'+str(i)],
+                                                  'attention_reweight_'+str(i), 'PROD'))
+        bottom_list.append('attention_reweight_'+str(i))
+    f.write(gen_layer.generate_eltwise_layer_str('attention_reweight_sum', bottom_list, 'attention_reweight_sum', 'SUM'))
+
+
+def gen_classification_layer(f, num_output=64, num_class=num_class, dropout_ratio=0.7):
+    f.write(gen_layer.generate_flatten_layer_str('attention_reweight_flatten', 'attention_reweight_sum', 'attention_reweight_flatten'))
+    f.write(gen_layer.generate_dropout_layer_str('attention_reweight_dropout', 'attention_reweight_flatten', 'attention_reweight_dropout', dropout_ratio))
+    f.write(gen_layer.generate_fc_layer_str('classification_fc1', 'attention_reweight_dropout', 'classification_fc1', num_output))
+    f.write(gen_layer.generate_dropout_layer_str('classification_dropout', 'classification_fc1', 'classification_dropout', dropout_ratio))
+    f.write(gen_layer.generate_fc_layer_str('classification_fc2', 'classification_dropout', 'classification_fc2', num_class))
+    f.write(gen_layer.generate_softmax_loss_str('classification_loss', 'classification_fc2', 'label', 'classification_loss'))
+    f.write(gen_layer.generate_accuracy_str('classification_accuracy', 'classification_fc2', 'label', 'classification_accuracy'))
+
+
+def gen_net():
+    with open('train_test_net.prototxt', 'w') as f:
+        gen_data_layer(f)
+        gen_trans_layer(f)
+        gen_dense_layer(f)
+        gen_attention_layer(f)
+        gen_classification_layer(f)
+
+if __name__ == '__main__':
+    gen_net()
+
+
